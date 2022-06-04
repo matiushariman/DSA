a385811774bdbb15ce652fb6ccd174cfc9a17270
Matius Hariman Natasampurna <matius.hariman@gmail.com>
2022-06-04 14:19:24 +0000

    :sparkles: add prepend method to SinglyLinkedList

--- .build/LinkedList/SinglyLinkedList.js
+++ .build/LinkedList/SinglyLinkedList.js
@@ -1,10 +1,16 @@
 const SinglyLinkedList = (value) => {
-  const head = {
+  let head = {
     value,
     next: null
   };
   let tail = head;
   let length = 1;
+  function Node(value2) {
+    return {
+      value: value2,
+      next: null
+    };
+  }
   function get() {
     return {
       head,
@@ -13,22 +19,28 @@ const SinglyLinkedList = (value) => {
     };
   }
   function append(value2) {
-    const newNode = {
-      value: value2,
-      next: null
-    };
+    const newNode = Node(value2);
     tail.next = newNode;
     tail = newNode;
     length++;
   }
   ;
+  function prepend(value2) {
+    const newNode = Node(value2);
+    newNode.next = head;
+    head = newNode;
+    length++;
+  }
   return {
     append,
-    get
+    get,
+    prepend
   };
 };
 const myLinkedList = SinglyLinkedList(10);
 myLinkedList.append(5);
 myLinkedList.append(16);
+console.log(myLinkedList.get());
+myLinkedList.prepend(1);
 console.log(myLinkedList.get());
 //# sourceMappingURL=SinglyLinkedList.js.map
--- LinkedList/SinglyLinkedList.ts
+++ LinkedList/SinglyLinkedList.ts
@@ -1,35 +1,56 @@
-type LinkedListNode = {
+type SinglyLinkedListNode = {
   value: number;
-  next: LinkedListNode | null;
+  next: SinglyLinkedListNode | null;
+};
+
+type GetSinglyLinkedList = {
+  head: SinglyLinkedListNode; 
+  tail: SinglyLinkedListNode;
+  length: number;
 };
 
 const SinglyLinkedList = (value: number) => {
-  const head:LinkedListNode = {
+  let head:SinglyLinkedListNode = {
     value,
     next: null,
   };
   let tail = head;
   let length = 1;
 
-  function get(): { head: LinkedListNode, tail: LinkedListNode, length: number } {
+  function Node(value:number): SinglyLinkedListNode {
+    return {
+      value,
+      next: null,
+    };  
+  }
+  
+  /** return current state of SinglyLinkedList **/
+  function get(): GetSinglyLinkedList {
     return {
       head, tail, length,
     };
   }
-  
+
+  /** add new value to the end of SinglyLinkedList **/
   function append(value: number): void {
-    const newNode = {
-      value,
-      next: null,
-    };
+    const newNode = Node(value);
     tail.next = newNode;
     tail = newNode;
     length++;
   };
 
+  /** add new value to the start of SinglyLinkedList  **/
+  function prepend(value:number): void {
+    const newNode = Node(value);
+    newNode.next = head;
+    head = newNode;
+    length++;
+  }
+  
   return {
     append,
     get,
+    prepend,
   };
 };
 
@@ -38,4 +59,7 @@ const myLinkedList = SinglyLinkedList(10);
 myLinkedList.append(5);
 myLinkedList.append(16);
 // 10 -> 5 -> 16
+console.log(myLinkedList.get());
+myLinkedList.prepend(1);
+// 1 -> 10 -> 5 -> 16
 console.log(myLinkedList.get());
