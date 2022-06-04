1b914c50dd6a05da3c5ce7f5a9d59288fbe97916
Matius Hariman Natasampurna <matius.hariman@gmail.com>
2022-06-04 15:20:40 +0000

    :sparkles: add insert & remove method

--- .build/LinkedList/SinglyLinkedList.js
+++ .build/LinkedList/SinglyLinkedList.js
@@ -11,18 +11,22 @@ const SinglyLinkedList = (value) => {
       next: null
     };
   }
-  function get() {
-    return {
-      head,
-      tail,
-      length
-    };
+  function printList() {
+    const arrNodes = [];
+    let currentNode = head;
+    while (currentNode !== null) {
+      arrNodes.push(currentNode.value);
+      currentNode = currentNode.next;
+    }
+    console.log(arrNodes);
   }
   function append(value2) {
     const newNode = Node(value2);
     tail.next = newNode;
     tail = newNode;
     length++;
+    console.log(`After appending ${value2}:`);
+    printList();
   }
   ;
   function prepend(value2) {
@@ -30,17 +34,50 @@ const SinglyLinkedList = (value) => {
     newNode.next = head;
     head = newNode;
     length++;
+    console.log(`After prepending ${value2}:`);
+    printList();
+  }
+  function traverseToIndex(index) {
+    let currentNode = head;
+    let counter = 0;
+    while (counter !== index && currentNode.next !== null) {
+      currentNode = currentNode.next;
+      counter++;
+    }
+    return currentNode;
+  }
+  function insert(index, value2) {
+    if (index >= length) {
+      return append(value2);
+    }
+    const newNode = Node(value2);
+    const leader = traverseToIndex(index - 1);
+    const pointer = leader.next;
+    leader.next = newNode;
+    newNode.next = pointer;
+    length++;
+    console.log(`After inserting ${value2} to index ${index}:`);
+    printList();
+  }
+  function remove(index) {
+    const leader = traverseToIndex(index - 1);
+    const unwantedNode = leader.next;
+    leader.next = unwantedNode ? unwantedNode.next : null;
+    console.log(`After removing node at index ${index}:`);
+    printList();
   }
   return {
     append,
-    get,
-    prepend
+    printList,
+    prepend,
+    insert,
+    remove
   };
 };
 const myLinkedList = SinglyLinkedList(10);
 myLinkedList.append(5);
 myLinkedList.append(16);
-console.log(myLinkedList.get());
 myLinkedList.prepend(1);
-console.log(myLinkedList.get());
+myLinkedList.insert(2, 99);
+myLinkedList.remove(2);
 //# sourceMappingURL=SinglyLinkedList.js.map
--- LinkedList/SinglyLinkedList.ts
+++ LinkedList/SinglyLinkedList.ts
@@ -23,20 +23,29 @@ const SinglyLinkedList = (value: number) => {
       next: null,
     };  
   }
-  
-  /** return current state of SinglyLinkedList **/
-  function get(): GetSinglyLinkedList {
-    return {
-      head, tail, length,
-    };
-  }
 
+    /** print nodes value **/
+  function printList():void {
+    const arrNodes: number[] = [];
+    let currentNode: SinglyLinkedListNode | null = head;
+    
+    while (currentNode !== null) {
+      arrNodes.push(currentNode.value);
+      currentNode = currentNode.next;
+    }
+    
+    console.log(arrNodes);
+  }
+  
   /** add new value to the end of SinglyLinkedList **/
   function append(value: number): void {
     const newNode = Node(value);
     tail.next = newNode;
     tail = newNode;
     length++;
+
+    console.log(`After appending ${value}:`);
+    printList();
   };
 
   /** add new value to the start of SinglyLinkedList  **/
@@ -45,21 +54,69 @@ const SinglyLinkedList = (value: number) => {
     newNode.next = head;
     head = newNode;
     length++;
+
+    console.log(`After prepending ${value}:`);
+    printList();
+  }
+
+  /** traverse to node of specific index **/
+  function traverseToIndex(index: number) {
+    let currentNode: SinglyLinkedListNode | null = head;
+    let counter = 0;
+
+    while (counter !== index && currentNode.next !== null) {
+      currentNode = currentNode.next;
+      counter++;
+    }
+
+    return currentNode;
+  }
+  
+  /** add new value to specific index in SinglyLinkedList **/
+  function insert(index: number, value: number): void {
+    if (index >= length) {
+      return append(value);
+    }
+
+    const newNode = Node(value);
+    const leader = traverseToIndex(index - 1);
+    const pointer = leader.next;
+    leader.next = newNode;
+    newNode.next = pointer;
+    length++;
+
+    console.log(`After inserting ${value} to index ${index}:`);
+    printList();
+  }
+
+  /** remove value from specific index **/
+  function remove(index: number) {
+    const leader = traverseToIndex(index - 1);
+    const unwantedNode = leader.next;
+    leader.next = unwantedNode ? unwantedNode.next : null;
+    
+    console.log(`After removing node at index ${index}:`);
+    printList();
   }
   
   return {
     append,
-    get,
+    printList,
     prepend,
+    insert,
+    remove,
   };
 };
 
 
 const myLinkedList = SinglyLinkedList(10);
+// 10 -> 5
 myLinkedList.append(5);
-myLinkedList.append(16);
 // 10 -> 5 -> 16
-console.log(myLinkedList.get());
-myLinkedList.prepend(1);
+myLinkedList.append(16);
 // 1 -> 10 -> 5 -> 16
-console.log(myLinkedList.get());
+myLinkedList.prepend(1);
+// 1 -> 10 -> 99 -> 5 -> 16
+myLinkedList.insert(2, 99);
+myLinkedList.remove(2);
+
